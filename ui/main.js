2fd0f20cd45ebec1fcc31491f5ec6e612240f3e4
pathanjalisrinivasan <getpathanjali@gmail.com>
2016-10-25 23:07:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,15 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 var img= document.getElementById('image');
 img.onclick=function()
 {
   img.style.marginLeft='100px';  
-};
+};*/
+
+var button=document.getElementById('counter');
+var counter=0;
+
+Button.onclick=function(){
+    counter++;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+}
